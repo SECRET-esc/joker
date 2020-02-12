da24374e2022cec58adf6dbd92ca5706a396526c
a.krushinskiy <a.krushinskiy@pokerdom.com>
2020-02-12 17:09:31 +0200

    package - com.pd.pokerdom

---
+++ app/src/main/java/com/pd/pokerdom/MyFirebaseMessagingService.kt
@@ -0,0 +1,86 @@
+package com.pd.pokerdom
+
+import android.content.Intent
+import android.util.Log
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d(TAG, "From: " + remoteMessage.from)
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.data)
+            scheduleJob()
+        }
+        // Check if message contains a notification payload.
+        if (remoteMessage.notification != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.notification!!.body)
+        }
+
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+        sendRegistrationToServer(token)
+    }
+
+    private fun scheduleJob() {
+        val work = OneTimeWorkRequest.Builder(MyWorker::class.java)
+                .build()
+        WorkManager.getInstance(applicationContext).beginWith(work).enqueue()
+    }
+
+    private fun sendRegistrationToServer(token: String) {
+        pushToken = token
+        sendDataToActivity()
+    }
+
+    private fun sendDataToActivity() {
+        val i = Intent()
+        i.action = "PUSH_REGISTERED"
+        i.putExtra("PUSH", pushToken)
+        sendBroadcast(i)
+    }
+
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private fun sendNotification(messageBody: String) {
+//        val intent = Intent(this, MainActivity::class.java)
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+//        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+//                PendingIntent.FLAG_ONE_SHOT)
+//        val channelId = getString(R.string.default_notification_channel_id)
+//        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val channel = NotificationChannel(channelId,
+//                    "YOUR_CHANNEL_NAME",
+//                    NotificationManager.IMPORTANCE_DEFAULT)
+//            channel.description = "YOUR_NOTIFICATION_CHANNEL_DISCRIPTION"
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+//        val notificationBuilder = Notification.Builder(this, channelId)
+//                .setSmallIcon(R.mipmap.ic_launcher)
+//                .setContentTitle(getString(R.string.fcm_message))
+//                .setContentText(messageBody)
+//                .setAutoCancel(true)
+//                .setSound(defaultSoundUri)
+//                .setContentIntent(pendingIntent)
+//        // Since android Oreo notification channel is needed.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val channel = NotificationChannel(channelId,
+//                    "Channel human readable title",
+//                    NotificationManager.IMPORTANCE_DEFAULT)
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+//    }
+
+    companion object {
+        private const val TAG = "MyFirebaseMsgService"
+        var pushToken: String? = null
+    }
+}
